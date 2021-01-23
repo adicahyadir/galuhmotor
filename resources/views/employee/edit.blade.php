8d009e4698652076d0f413b0cf28dd806458f98e
richoandika <richoandika@null.net>
2021-01-23 15:52:50 +0700

    Update edit.blade.php

--- resources/views/employee/edit.blade.php
+++ resources/views/employee/edit.blade.php
@@ -16,14 +16,23 @@
             <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Nama</span>
                 <input id="name" name="name" value="{{ $infoUser->name }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="John Doe"/>
+                @error('name')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Alamat</span>
                 <textarea name="address" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray" rows="3" placeholder="Jl. Anggur No. 10">{{ $infoUser->address }}</textarea>
+                @error('address')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">No. Hp</span>
                 <input id="phone" name="phone" value="{{ $infoUser->phone }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="08XXXXXXXXXX"/>
+                @error('phone')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 mb-2 text-sm">
                 <span class="text-gray-700 dark:text-gray-400"> Jabatan </span>
