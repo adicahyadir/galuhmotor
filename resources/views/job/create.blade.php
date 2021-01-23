901ab6f6ec10035856e0102e7b23c4278c637421
richoandika <richoandika@null.net>
2021-01-23 15:53:36 +0700

    Update create.blade.php

--- resources/views/job/create.blade.php
+++ resources/views/job/create.blade.php
@@ -15,6 +15,9 @@
             <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Nama</span>
                 <input id="name" name="name" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Admin"/>
+                @error('name')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
         </div>
         <div class="flex mt-4 items-center justify-between">
