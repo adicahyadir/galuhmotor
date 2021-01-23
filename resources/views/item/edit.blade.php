b78452286a185f5ea77abcd3ad7eece12d0f62ee
richoandika <richoandika@null.net>
2021-01-23 15:52:04 +0700

    Update edit.blade.php

--- resources/views/item/edit.blade.php
+++ resources/views/item/edit.blade.php
@@ -16,14 +16,23 @@
             <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Nama</span>
                 <input id="name" name="name" value="{{ $data->name }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="John Doe"/>
+                @error('name')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Jumlah</span>
                 <input type="number" id="qty" name="qty" value="{{ $data->jumlah }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="7"/>
+                @error('qty')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Harga Per Unit</span>
                 <input type="number" id="price" name="price" value="{{ $data->price }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="200000"/>
+                @error('price')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 mb-2 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Kategori</span>
