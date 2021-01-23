3faf601c91bdca4a820596813b50ee816555a20a
richoandika <richoandika@null.net>
2021-01-23 15:55:48 +0700

    Update edit.blade.php

--- resources/views/supplier/edit.blade.php
+++ resources/views/supplier/edit.blade.php
@@ -16,14 +16,23 @@
             <label class="block text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Nama</span>
                 <input id="name" name="name" value="{{ $data->name }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="John Doe"/>
+                @error('name')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">No. Hp</span>
                 <input id="phone" name="phone" value="{{ $data->phone }}" class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="08XXXXXXXXXX"/>
+                @error('phone')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
             <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">Keterangan</span>
                 <textarea name="descriptions" class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray" rows="6" placeholder="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt illo eius molestiae maxime, doloribus magnam dolorum, dolor labore facere amet molestias vitae eos fugiat! Neque sapiente saepe, ab at repudiandae harum asperiores facere, quaerat dolorem ea commodi recusandae similique voluptas, eaque ut odio earum consequatur ad unde iste nobis non.">{{ $data->descriptions }}</textarea>
+                @error('descriptions')
+                <span class="text-xs text-red-600 dark:text-red-400">{{ $message }}</span>
+                @enderror
             </label>
         </div>
         <div class="flex mt-4 items-center justify-between">
