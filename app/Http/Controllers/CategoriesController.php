8e94fd9397004f51571eb7609098698731a23f40
richoandika <richoandika@null.net>
2021-01-23 15:50:55 +0700

    Update CategoriesController.php

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -50,7 +50,7 @@ class CategoriesController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required',
+            'name' => 'required|unique:categories',
         ]);
 
         Categories::create([
